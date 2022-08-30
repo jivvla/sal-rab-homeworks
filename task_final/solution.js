7102bdacec25e0b35cdfd6b98345e2b9a29568aa
Alexandra <sasha0310@inbox.ru>
2022-08-30 15:49:27 +0300

    12

--- task_final/solution.js
+++ task_final/solution.js
@@ -35,9 +35,8 @@
 
 function sendRequest(name, phone, address, goods, sum) {
     //let data = {goods: [], order: {}};
-    let data = {client: client, order:{address:`${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`,sum}, goods:[{title:title, count:count}]};
+    let data = {client: `${name} ${phone}`, order:{address:`${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`,sum}, goods:[{}]};
     
-    data.client = `${name} ${phone}`;
     //data.order.address = `${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor}, кв ${address.flat}`;
     data.order.sum = sum;
 
