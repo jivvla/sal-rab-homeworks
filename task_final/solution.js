cc974bbcb804749c9f50efa118755aecd4367a5d
Alexandra <sasha0310@inbox.ru>
2022-08-30 16:19:02 +0300

    18

--- task_final/solution.js
+++ task_final/solution.js
@@ -43,7 +43,7 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
     
    for (let i = 0; i < countOfGoods; i += 1) {
-       data.goods.push(goods[i].title);
+       data.goods.push(goods[i][title, count]);
  //      data.goods[i].title = goods[i].title;
   //     data.goods[i].count = goods[i].count;
     }
